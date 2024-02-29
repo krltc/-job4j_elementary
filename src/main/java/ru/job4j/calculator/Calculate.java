294ca984738b7023c703737c0be84c410bd2a0d7
krltc <o.my.life@mail.ru>
2024-02-29 20:46:13 +0300

    Что такое переменная. [#3610]

---
+++ src/main/java/ru/job4j/calculator/Calculate.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+public class Calculate {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println(age);
+        int age1 = 22;
+        System.out.println(age1);
+        int age2 = 34;
+        System.out.println(age2);
+        int age3 = 36;
+        System.out.println(age3);
+        int age4 = 45;
+        System.out.println(age4);
+        int age5 = 55;
+        System.out.println(age5);
+        int age6 = 67;
+        System.out.println(age6);
+        float lenght = 78.0332F;
+        System.out.println(lenght);
+        float length = 78.0332F;
+        System.out.println(length);
+        boolean baby = true;
+        System.out.println(baby);
+    }
+}

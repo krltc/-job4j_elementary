5b1830f502ad25c964da223aea9b2471413a298b
krltc <o.my.life@mail.ru>
2024-03-06 20:48:15 +0300

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/calculator/ArgumentsMethod.java
+++ src/main/java/ru/job4j/calculator/ArgumentsMethod.java
@@ -7,7 +7,7 @@ public class ArgumentsMethod {
 
     public static void main(String[] args) {
         String name = "Job4j";
-        int age = 7;
+        int age = 6;
         ArgumentsMethod.hello(name, age);
         ArgumentsMethod.hello(name, age);
         ArgumentsMethod.hello(name, age);
